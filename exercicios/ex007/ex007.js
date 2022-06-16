60d8f629daee527536bba68c9b00ecbe9d26dc8f
Anna Cristina <106214062+anncristinna@users.noreply.github.com>
2022-06-15 23:37:19 -0300

    fiz os exercicios

---
+++ exercicios/ex007/ex007.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`Sua velocidade é de ${vel}KM/H`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida.Multado!')
+}
+console.log('Dirija sempre com cinto de segurança')
